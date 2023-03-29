d1b0c719d43005bf0da0c78a45a4e437368adb87
Manoleau <emmanuelardoin@gmail.com>
2023-03-29 16:22:19 +0200

    Test de lire un fichier CSV (echoue)

--- GrapheBelka/src/TestGraphe.java
+++ GrapheBelka/src/TestGraphe.java
@@ -1,8 +1,8 @@
-import java.io. * ;
 import java.util.Scanner;
+import java.io.*;
+
 public class TestGraphe {
     private static LCGraphe newGraphe = new LCGraphe();
-    private static File file = new File("fileTest.csv");
     public static void main(String [] args ) throws ExistEdgeException {
         newGraphe.addMain("Lyon", "Bloc");
         newGraphe.addMain("Paris", "M");
@@ -12,28 +12,7 @@ public class TestGraphe {
         } catch (ExistEdgeException e){
             System.out.println(e.getMessage());
         }
-        
         System.out.println(newGraphe.existEdge("Lyon", "Paris"));
         System.out.println(newGraphe);
-        Scanner ent = new Scanner(file);
-
-        // String fichierCSV = "../../Fichiers pour Etudiants/liste-adjacence-jeuEssai.csv";
-        // String ligne = "";
-        // String separateur = ";";
-
-        // try (BufferedReader br = new BufferedReader(new FileReader(fichierCSV))) {
-
-        //     while ((ligne = br.readLine()) != null) {
-
-        //         String[] donnees = ligne.split(separateur);
-
-        //         // Utilisez les données lues ici
-        //         System.out.println("Colonne 1 : " + donnees[0] + " | Colonne 2 : " + donnees[1]);
-
-        //     }
-
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
     }
 }
