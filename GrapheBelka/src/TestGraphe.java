03a7966f041c0d5446fc78e9cf0f8742af506801
Manoleau <emmanuelardoin@gmail.com>
2023-03-31 12:34:35 +0200

    .

--- GrapheBelka/src/TestGraphe.java
+++ GrapheBelka/src/TestGraphe.java
@@ -3,16 +3,24 @@ import java.io.*;
 
 public class TestGraphe {
     private static LCGraphe newGraphe = new LCGraphe();
-    public static void main(String [] args ) throws ExistEdgeException {
-        newGraphe.addMain("Lyon", "Bloc");
-        newGraphe.addMain("Paris", "M");
-        newGraphe.addEdge("Lyon", "Paris", 4, 6, 5);
+    public static void main(String [] args ) throws ExistEdgeException , NotExistMainException{
+        newGraphe.ajoutSommet("Lyon", "Bloc");
+        newGraphe.ajoutSommet("Paris", "JSP");
+        newGraphe.ajoutSommet("Pagnoz", "Clinique");
+        newGraphe.ajoutVoisin("Lyon", "Paris", 4, 6, 5);
+        
         try{
-            newGraphe.addEdge("Lyon", "Paris", 4, 6, 5);
+            // newGraphe.ajoutVoisin("Lyon", "ZEUB", 4, 6, 5);
+            newGraphe.ajoutVoisin("Lyon", "Pagnoz", 3, 3, 2);
+            newGraphe.ajoutVoisin("Lyon", "Paris", 4, 6, 5);
         } catch (ExistEdgeException e){
             System.out.println(e.getMessage());
         }
-        System.out.println(newGraphe.existEdge("Lyon", "Paris"));
-        System.out.println(newGraphe);
+        // newGraphe.modifVoisin("Paris", "Lyon", 0, 0, 0);
+        System.out.println(newGraphe.existeVoisin("Lyon", "Paris"));
+        System.out.println(newGraphe.getVoisins("Pagnoz").get(0).getDestination());
+        // System.out.println();
+        System.out.println(newGraphe.getCentre("Lyon").afficherVoisins());
+        // System.out.println(newGraphe.tousLesCentres());
     }
 }
