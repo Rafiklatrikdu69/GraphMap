4f8e8c7ae356f467fdcf13f8b43dc458be316903
ARDOIN EMMANUEL p2202643 <emmanuel.ardoin@etu.univ-lyon1.fr>
2023-05-05 15:22:30 +0200

    Main

--- Graphe/src/main/java/Main.java
+++ Graphe/src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main extends Exception {
 
     public static void main(String[] args) throws NotExistMainException {
         newGraphe.chargementFichier();
-        System.out.println(newGraphe.plusCourtCheminDijkstrat("S1", "S4"));
-
+        System.out.println(newGraphe.plusCourtCheminDijkstrat("S1", "S15"));
+        System.out.println(newGraphe.plusCourtCheminDijkstra("S1", "S15"));
     }
 }
 
