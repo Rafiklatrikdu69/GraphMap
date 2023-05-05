623047088877a19dd15fa6c4b838df06884773d0
ARDOIN EMMANUEL p2202643 <emmanuel.ardoin@etu.univ-lyon1.fr>
2023-05-05 13:06:48 +0200

    Autre

--- Graphe/src/main/java/Main.java
+++ Graphe/src/main/java/Main.java
@@ -1,19 +1,14 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.io.File;
 import java.lang.String;
 
 public class Main extends Exception {
     private static LCGraphe newGraphe = new LCGraphe();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotExistMainException {
+        newGraphe.chargementFichier();
 
-        newGraphe.LectureFichier();
-
-        System.out.println(newGraphe.toString());
+        System.out.println(newGraphe.existeVoisin("S1", "S3"));
+        System.out.println();
+        newGraphe.affiche();
 
     }
 }
