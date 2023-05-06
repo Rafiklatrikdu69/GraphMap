cf20f8b3ac63f03a790910c668c692fc09db6233
ARDOIN EMMANUEL p2202643 <emmanuel.ardoin@etu.univ-lyon1.fr>
2023-05-06 20:23:26 +0200

    .

--- Graphe/src/main/java/MenuConsoleGraphe.java
+++ Graphe/src/main/java/MenuConsoleGraphe.java
@@ -3,7 +3,7 @@ import java.lang.String;
 import java.util.*;
 import java.util.Scanner;
 
-public class MenuConsoleGraphe extends Exception {
+public class MenuConsoleGraphe{
     private static LCGraphe newGraphe = new LCGraphe();
 
     public static void main(String[] args){
