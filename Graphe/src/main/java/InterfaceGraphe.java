af2e9f257782eb450b14c6da76989e85392a476c
unknown <rafik.bouchenna@etu.univ-lyon1.fr>
2023-05-07 15:27:46 +0200

    interface continuer

--- Graphe/src/main/java/InterfaceGraphe.java
+++ Graphe/src/main/java/InterfaceGraphe.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
@@ -12,61 +13,91 @@ import javax.swing.JPanel;
 public class InterfaceGraphe extends JFrame {
 
     private static LCGraphe Graphe = new LCGraphe();
-
+    
     private JMenuBar menu;
     private JMenu j;
-    private JMenuItem option1,option2,option3;
+    private JMenuItem option1, option2, option3;
     private JPanel panel, cp;
 
     public InterfaceGraphe() {
         super();
         Graphe.chargementFichier();
+        Graphe.toString();
         initComponents();
-        initEventListeners();
+        //initEventListeners();
         setTitle("Graphe");
-        setSize(1280,720);
+        setSize(1280, 720);
         setPreferredSize(new Dimension(1280, 720));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
         pack();
     }
 
     private void initComponents() {
         cp = (JPanel) getContentPane();
-        cp.setLayout(new FlowLayout());
+        System.out.println("test");
+       
         menu = new JMenuBar();
         j = new JMenu("menu");
         option1 = new JMenuItem("Afficher tous les dispensaires");
         option2 = new JMenuItem("Sélectionner un dispensaire");
         option3 = new JMenuItem("option 3");
-        j.add(option1);   
+        j.add(option1);
         j.add(option2);
         j.add(option3);
         menu.add(j);
         setJMenuBar(menu);
-    }
-    private void initEventListeners(){
+        cp.add(menu);
+        DessinGraphe dessin = new DessinGraphe(30, 100);
 
+        // Ajout de l'objet DessinGraphe au JPanel cp
+      
+        
     }
+
+    
 
     class DessinGraphe extends LCGraphe {
-        private double x ;
+
+        private double x;
         private double y;
-        
-        private MaillonGraphe premier;
-        private MaillonGraphe suiv;
-        DessinGraphe(double axeX, double axeY){
+
+        private LCGraphe.MaillonGraphe premier;
+        private LCGraphe.MaillonGraphe suiv=null;
+
+        DessinGraphe(double axeX, double axeY) {
             super();
             this.x = axeX;
             this.y = axeY;
+            premier = null;
+
         }
 
-       
+   public void dessinerSommet(Graphics2D g2d, MaillonGraphe sommet) {
+    int rayon = 20; // rayon du cercle représentant le sommet
+    int x = (int) (this.x);
+    int y = (int) (this.y);
 
-       
+    g2d.setColor(Color.BLUE);
+    g2d.fillOval(x - rayon / 2, y - rayon / 2, rayon, rayon); // dessin du cercle
+    g2d.setColor(Color.BLACK);
+    g2d.drawOval(x - rayon / 2, y - rayon / 2, rayon, rayon); // contour du cercle
+}
 
-        
+
+
+public void dessin(JPanel panel) {
+    Graphics2D g2d = (Graphics2D) panel.getGraphics();
+    LCGraphe.MaillonGraphe tmp = this.premier;
+    while (tmp != null) {
+        System.out.println(tmp);
+        dessinerSommet(g2d, tmp);
+        tmp = tmp.suiv;
+    }
+}
+
     }
 
 }
