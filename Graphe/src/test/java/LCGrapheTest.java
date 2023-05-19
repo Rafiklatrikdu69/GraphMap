cc15af98996a2a8ac02894f88a1e477d9c00ac0a
Rafik <rafik.bouchenna@etu.univ-lyon1.fr>
2023-05-19 18:31:23 +0200

    t

--- Graphe/src/test/java/LCGrapheTest.java
+++ Graphe/src/test/java/LCGrapheTest.java
@@ -2,13 +2,28 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class LCGrapheTest extends TestCase {
-    @Test
 
-    public void test() {
+    @Test
+    public void testExisteCentre() {
         LCGraphe graphe = new LCGraphe();
+        graphe.chargementFichier("chemin_vers_le_fichier.csv");
+
+        assertFalse(graphe.existeCentre("S30"));
         assertNull(graphe.existeCentre("S33"));
+        // Autres assertions pour tester différents cas
+    }
+
+    @Test
+    public void testAjouterSommet() {
+        LCGraphe graphe = new LCGraphe();
+
+
+        assertTrue(graphe.existeCentre("S1"));
+        assertTrue(graphe.existeCentre("S2"));
+        assertFalse(graphe.existeCentre("S3"));
 
     }
+
 
 
 }
