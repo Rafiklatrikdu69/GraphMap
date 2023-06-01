b502e5efc75ab20d9d19648e8d334dc89a39d89e
ARDOIN EMMANUEL p2202643 <emmanuel.ardoin@etu.univ-lyon1.fr>
2023-06-01 21:41:18 +0200

    Ajout Dark/Light mode

--- Graphe/src/main/java/LancerInterface.java
+++ Graphe/src/main/java/LancerInterface.java
@@ -1,11 +1,18 @@
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
+import javax.swing.*;
+
 
 public class LancerInterface {
     public static void main(String[] args){
-        new InterfaceGraphe();
-        FlatMacLightLaf.setup();
-        
+
+        try {
+            // Changer le thème en utilisant le nom de classe du look and feel
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+            new InterfaceGraphe();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
